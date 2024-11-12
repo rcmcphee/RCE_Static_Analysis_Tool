4433a6bd56cb96c41a77f931be9883250fdb5e02
Ryan McPhee <rcmcphee@ncsu.edu>
2024-11-11 23:22:38 -0500

    initial analysis code

--- analyze.py
+++ analyze.py
@@ -3,7 +3,15 @@ import os
 
 # Analyze the contents of the file for RCE vulnerabilities
 def analyze(contents):
+
+    potential_vuln = []
     
     tree = javalang.parse.parse(contents)
 
-    exit
+    for path, node in tree.filter(javalang.tree.MethodInvocation):
+        methods = f"{node.qualifier}.{node.member}" if node.qualifier else node.member
+        if methods in ["exec", "getRuntime", "invoke", "lookup", "newInstance", "ObjectInputStream"]:
+            if any(argument for argument in node.arguments if "input" in str(argument)):
+                potential_vuln.append((methods, node.position.line))
+    
+    return potential_vuln
